ef48cc284c275208c4e0c77feaebfe58998ba10e
hamedmoody <hamedmoodi2011@gmail.com>
2019-05-29 16:28:00 +0430

    Add raychat class to body when raychat has been loaded

--- templates/script.php
+++ templates/script.php
@@ -13,6 +13,7 @@ if( is_user_logged_in() ) {
 	!function(){function t(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,localStorage.getItem("rayToken")?t.src="https://app.raychat.io/scripts/js/"+o+"?rid="+localStorage.getItem("rayToken")+"&href="+window.location.href:t.src="https://app.raychat.io/scripts/js/"+o;var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e)}var e=document,a=window,o="<?php echo $widget_id; ?>";"complete"==e.readyState?t():a.attachEvent?a.attachEvent("onload",t):a.addEventListener("load",t,!1)}();
 	<?php if( $user ):?>
 	window.addEventListener('raychat_ready', function (ets) {
+		document.body.classList.add("raychat");
 		window.Raychat.setUser({
 		   email 		: '<?php echo $user->user_email;?>',
 		   name 		: '<?php echo $user->display_name;?>',
